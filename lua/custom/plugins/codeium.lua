986f918dddb6f1ef418781da051069203a3ce16b
Khaydanov Yuriy <UnLess24@users.noreply.github.com>
2023-11-22 15:41:12 +0300

    Codeium: change keys and they behavior

--- lua/custom/plugins/codeium.lua
+++ lua/custom/plugins/codeium.lua
@@ -5,12 +5,12 @@ return {
 		vim.g.codeium_disable_bindings = 1
 
 		vim.keymap.set('i', '<leader><tab>', function() return vim.fn['codeium#Accept']() end,
-			{ desc = 'Codeium: Accept', expr = true })
+			{ desc = 'Codeium: Accept', expr = true, noremap = true })
 		vim.keymap.set('i', '<leader><C-j>', function() return vim.fn['codeium#CycleCompletions'](1) end,
-			{ desc = 'Codeium: Next suggestion', expr = true })
+			{ desc = 'Codeium: Next suggestion', expr = true, noremap = true })
 		vim.keymap.set('i', '<leader><C-k>', function() return vim.fn['codeium#CycleCompletions'](-1) end,
-			{ desc = 'Codeium: Previous suggestion', expr = true })
-		vim.keymap.set('i', '<leader><C-x>', function() return vim.fn['codeium#Clear']() end,
-			{ desc = 'Codeium: Clear suggestion', expr = true })
+			{ desc = 'Codeium: Previous suggestion', expr = true, noremap = true })
+		vim.keymap.set('i', '<leader><space>', function() return vim.fn['codeium#Clear']() and '<space>' end,
+			{ desc = 'Codeium: continue without suggestion', expr = true, noremap = true })
 	end,
 }
