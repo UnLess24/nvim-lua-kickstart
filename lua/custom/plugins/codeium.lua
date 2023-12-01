b4bb3d3853dc4a9b0e7952e52547f421a479f7fa
Khaydanov Yuriy <UnLess24@users.noreply.github.com>
2023-12-01 10:59:53 +0300

    Codeium: manual mode, change keys

--- lua/custom/plugins/codeium.lua
+++ lua/custom/plugins/codeium.lua
@@ -3,15 +3,18 @@ return {
 	event = 'BufEnter',
 	config = function()
 		vim.g.codeium_disable_bindings = 1
-		vim.g.codeium_enabled = 0
+		-- vim.g.codeium_enabled = 0
+		vim.g.codeium_manual = 1
 
-		vim.keymap.set('i', '<C-/><tab>', function() return vim.fn['codeium#Accept']() end,
-			{ desc = 'Codeium: Accept', expr = true, noremap = true })
-		vim.keymap.set('i', '<C-/>j', function() return vim.fn['codeium#CycleCompletions'](1) end,
-			{ desc = 'Codeium: Next suggestion', expr = true, noremap = true })
-		vim.keymap.set('i', '<C-/>k', function() return vim.fn['codeium#CycleCompletions'](-1) end,
-			{ desc = 'Codeium: Previous suggestion', expr = true, noremap = true })
-		vim.keymap.set('i', '<C-/><space>', function() return vim.fn['codeium#Clear']() end,
-			{ desc = 'Codeium: continue without suggestion', expr = true, noremap = true })
+		vim.keymap.set('i', '<C-q><tab>', function() return vim.fn['codeium#Accept']() end,
+			{ desc = 'Accept', expr = true, noremap = true })
+		vim.keymap.set('i', '<C-q>c', function() return vim.fn['codeium#Complete']() end,
+			{ desc = 'Do suggestion', expr = true, noremap = true })
+		vim.keymap.set('i', '<C-q>j', function() return vim.fn['codeium#CycleCompletions'](1) end,
+			{ desc = 'Next suggestion', expr = true, noremap = true })
+		vim.keymap.set('i', '<C-q>k', function() return vim.fn['codeium#CycleCompletions'](-1) end,
+			{ desc = 'Previous suggestion', expr = true, noremap = true })
+		-- vim.keymap.set('i', '<C-q><space>', function() return vim.fn['codeium#Clear']() end,
+		-- 	{ desc = 'continue without suggestion', expr = true, noremap = true })
 	end,
 }
