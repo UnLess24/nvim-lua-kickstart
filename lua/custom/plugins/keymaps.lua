ef5335ef0f424309b84bdf19b8af16af846c103a
Khaydanov Yuriy <UnLess24@users.noreply.github.com>
2024-02-01 09:36:59 +0300

    Add some keymaps

--- lua/custom/plugins/keymaps.lua
+++ lua/custom/plugins/keymaps.lua
@@ -1,4 +1,9 @@
 return {
 	-- Set jk to hit esc key
 	vim.api.nvim_set_keymap('i', 'jk', "<esc>", { desc = 'Alias to ESC key for edit', noremap = true }),
+
+	vim.api.nvim_set_keymap('', '+', "<C-a>", { desc = 'increment' }),
+	vim.api.nvim_set_keymap('', '-', "<C-x>", { desc = 'decrement' }),
+
+	vim.api.nvim_set_keymap('n', "<C-a>", 'ggVG', { desc = 'Select all', noremap = true }),
 }
